6fb503086538f94bf1c7d3dbc6935e9db9cbf255
BHB <bela127@users.noreply.github.com>
2017-10-30 22:29:04 +0100

    formating

--- src/demo/Anzeige.java
+++ src/demo/Anzeige.java
@@ -1,4 +1,5 @@
 package demo;
+
 import java.awt.Point;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -12,43 +13,42 @@ import simulation.Simulation;
 import simulation.SimulationState;
 
 public class Anzeige {
-	
 
 	public static void main(String[] args) {
-		
+
 		Path initPath = Paths.get(args[0], args[1]);
 		Path inPath = Paths.get(args[0], args[2]);
-		
+
 		Simulation sim = initSim(initPath.toString());
-		
-		String simString = runSim(sim,inPath.toString());
-		
+
+		String simString = runSim(sim, inPath.toString());
+
 		int score = sim.getScore();
-		
+
 		System.out.println(simString);
-		
+
 		displaySim(simString);
-		
+
 		System.out.println(score);
 	}
-	
+
 	static void displaySim(String simString) {
 		Scanner scanner = new Scanner(simString);
 		String line = "";
-		
+
 		line = scanner.nextLine();
 		int[] size = SimulationState.stringToInt(line.split(" "));
-		DisplayWindow window = new DisplayWindow(size[0],size[1]);
-		
+		DisplayWindow window = new DisplayWindow(size[0], size[1]);
+
 		StringBuilder simState = new StringBuilder();
-		while(scanner.hasNextLine()) {
+		while (scanner.hasNextLine()) {
 			line = scanner.nextLine();
-			if(!line.equals("#")) {
+			if (!line.equals("#")) {
 				simState.append(line + "\n");
-			}else{
+			} else {
 				window.drawSimulationState(simState.toString());
 				simState = new StringBuilder();
-				
+
 				try {
 					Thread.sleep(100);
 				} catch (InterruptedException e1) {
@@ -57,93 +57,91 @@ public class Anzeige {
 				}
 			}
 		}
-		
+
 		scanner.close();
 	}
-	
-	
+
 	static String runSim(Simulation sim, String playerInputPath) {
 		File file = new File(playerInputPath);
-        System.out.println(file.getAbsolutePath());
-        
-        Scanner input;
+		System.out.println(file.getAbsolutePath());
+
+		Scanner input;
 		try {
 			input = new Scanner(file);
 			String line = "";
-			
 
-		
-		StringBuilder simSB = new StringBuilder();
-		simSB.append(sim.getSimulationState().width + " " + sim.getSimulationState().height + "\n");
-		
-		System.out.println("Loop");
-		int steps = 0;
-		while(steps < 200 && input.hasNextLine()) {
-			String state = sim.getSimulationStateString();
-			simSB.append(state);
-			simSB.append("#\n");
-			
-			try {
-				line = input.nextLine();
-					if(line.equals("true"))sim.update(true);
+			StringBuilder simSB = new StringBuilder();
+			simSB.append(sim.getSimulationState().width + " " + sim.getSimulationState().height + "\n");
+
+			System.out.println("Loop");
+			int steps = 0;
+			while (steps < 200 && input.hasNextLine()) {
+				String state = sim.getSimulationStateString();
+				simSB.append(state);
+				simSB.append("#\n");
+
+				try {
+					line = input.nextLine();
+					if (line.equals("true"))
+						sim.update(true);
 					else {
 						int[] playerPos = SimulationState.stringToInt(line.split(" "));
-						sim.update(new Point(playerPos[0],playerPos[1]));
+						sim.update(new Point(playerPos[0], playerPos[1]));
 					}
-			} catch (Simulation.GameOver e) {
-				e.printStackTrace();
-				break;
+				} catch (Simulation.GameOver e) {
+					e.printStackTrace();
+					break;
+				}
+
+				steps++;
 			}
-			
-			steps++;
-		}
-		
-		input.close();
-		return simSB.toString();
-		
+
+			input.close();
+			return simSB.toString();
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return null;
-		}		
+		}
 	}
-	
+
 	static Simulation initSim(String simFilePath) {
 		System.out.println("Init");
-		
+
 		File file = new File(simFilePath);
-        System.out.println(file.getAbsolutePath());
+		System.out.println(file.getAbsolutePath());
 
-            Scanner input;
-			try {
-				input = new Scanner(file);
-				
-				String line = "";
-				
-				line = input.nextLine();
-				int[] size = SimulationState.stringToInt(line.split(" "));
-				Simulation sim = new Simulation(size[0],size[1]);
-				
-				line = input.nextLine();
-				int[] playerPos = SimulationState.stringToInt(line.split(" "));
-				Point player = new Point(playerPos[0],playerPos[1]);
-				sim.addPlayer(player);
-				
-				ArrayList<Point> zombies = new ArrayList<Point>();
-	            while (input.hasNextLine()) {
-	                line = input.nextLine();
-	                int[] zombiePos = SimulationState.stringToInt(line.split(" "));
-	                zombies.add(new Point(zombiePos[0],zombiePos[1]));
-	            }
-				sim.addZombies(zombies);
+		Scanner input;
+		try {
+			input = new Scanner(file);
 
-	            input.close();
-	            return sim;
-	            
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}		
+			String line = "";
+
+			line = input.nextLine();
+			int[] size = SimulationState.stringToInt(line.split(" "));
+			Simulation sim = new Simulation(size[0], size[1]);
+
+			line = input.nextLine();
+			int[] playerPos = SimulationState.stringToInt(line.split(" "));
+			Point player = new Point(playerPos[0], playerPos[1]);
+			sim.addPlayer(player);
+
+			ArrayList<Point> zombies = new ArrayList<Point>();
+			while (input.hasNextLine()) {
+				line = input.nextLine();
+				int[] zombiePos = SimulationState.stringToInt(line.split(" "));
+				zombies.add(new Point(zombiePos[0], zombiePos[1]));
+			}
+			sim.addZombies(zombies);
+
+			input.close();
+			return sim;
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 
--- src/demo/VorführungGen.java
+++ src/demo/VorführungGen.java
@@ -1,4 +1,5 @@
 package demo;
+
 import java.awt.Point;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -17,48 +18,48 @@ import simulation.Simulation.GameOver;
 import simulation.SimulationState;
 
 public class VorführungGen {
-	
-public static void main(String[] args) {
-		
+
+	public static void main(String[] args) {
+
 		Path initPath = Paths.get(args[0], args[1]);
 		Path inPath = Paths.get(args[0], args[2]);
 		Path outPath = Paths.get(args[0], args[3]);
-		
+
 		Simulation sim = initSim(initPath.toString());
-		
-		String simString = runSim(sim,inPath.toString());
-		
-		try{
+
+		String simString = runSim(sim, inPath.toString());
+
+		try {
 			File file = new File(outPath.toString());
-		    PrintWriter writer = new PrintWriter(file.getAbsolutePath(), "UTF-8");
-		    System.out.println(file.getAbsolutePath());
-		    writer.println(simString);
-		    writer.close();
+			PrintWriter writer = new PrintWriter(file.getAbsolutePath(), "UTF-8");
+			System.out.println(file.getAbsolutePath());
+			writer.println(simString);
+			writer.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		System.out.println(simString);
-		
+
 		displaySim(simString);
 	}
-	
+
 	static void displaySim(String simString) {
 		Scanner scanner = new Scanner(simString);
 		String line = "";
-		
+
 		line = scanner.nextLine();
 		int[] size = SimulationState.stringToInt(line.split(" "));
-		DisplayWindow window = new DisplayWindow(size[0],size[1]);
-		
+		DisplayWindow window = new DisplayWindow(size[0], size[1]);
+
 		StringBuilder simState = new StringBuilder();
-		while(scanner.hasNextLine()) {
+		while (scanner.hasNextLine()) {
 			line = scanner.nextLine();
-			if(!line.equals("#")) {
+			if (!line.equals("#")) {
 				simState.append(line + "\n");
-			}else{
+			} else {
 				window.drawSimulationState(simState.toString());
 				simState = new StringBuilder();
-				
+
 				try {
 					Thread.sleep(100);
 				} catch (InterruptedException e1) {
@@ -67,104 +68,101 @@ public static void main(String[] args) {
 				}
 			}
 		}
-		
+
 		scanner.close();
 	}
-	
-	
+
 	static String runSim(Simulation sim, String playerInputPath) {
-		try{
+		try {
 			File file = new File(playerInputPath);
-		    PrintWriter writer = new PrintWriter(file.getAbsolutePath(), "UTF-8");
-		    System.out.println(file.getAbsolutePath());
-		
-		
-		StringBuilder simSB = new StringBuilder();
-		simSB.append(sim.getSimulationState().width + " " + sim.getSimulationState().height + "\n");
-		
-		System.out.println("Loop");
-		int steps = 0;
-		while(steps < 200) {
-			String state = sim.getSimulationStateString();
-			simSB.append(state);
-			simSB.append("#\n");
-			
-			try {
-				loop(sim,writer);
-			} catch (Simulation.GameOver e) {
-				e.printStackTrace();
-				break;
+			PrintWriter writer = new PrintWriter(file.getAbsolutePath(), "UTF-8");
+			System.out.println(file.getAbsolutePath());
+
+			StringBuilder simSB = new StringBuilder();
+			simSB.append(sim.getSimulationState().width + " " + sim.getSimulationState().height + "\n");
+
+			System.out.println("Loop");
+			int steps = 0;
+			while (steps < 200) {
+				String state = sim.getSimulationStateString();
+				simSB.append(state);
+				simSB.append("#\n");
+
+				try {
+					loop(sim, writer);
+				} catch (Simulation.GameOver e) {
+					e.printStackTrace();
+					break;
+				}
+
+				steps++;
 			}
-			
-			steps++;
-		}
-		
-		writer.close();
-		return simSB.toString();
-		
+
+			writer.close();
+			return simSB.toString();
+
 		} catch (IOException e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
-	
+
 	static Simulation initSim(String simFilePath) {
 		System.out.println("Init");
-		
+
 		File file = new File(simFilePath);
-        System.out.println(file.getAbsolutePath());
+		System.out.println(file.getAbsolutePath());
 
-            Scanner input;
-			try {
-				input = new Scanner(file);
-				
-				String line = "";
-				
-				line = input.nextLine();
-				int[] size = SimulationState.stringToInt(line.split(" "));
-				Simulation sim = new Simulation(size[0],size[1]);
-				
-				line = input.nextLine();
-				int[] playerPos = SimulationState.stringToInt(line.split(" "));
-				Point player = new Point(playerPos[0],playerPos[1]);
-				sim.addPlayer(player);
-				
-				ArrayList<Point> zombies = new ArrayList<Point>();
-	            while (input.hasNextLine()) {
-	                line = input.nextLine();
-	                int[] zombiePos = SimulationState.stringToInt(line.split(" "));
-	                zombies.add(new Point(zombiePos[0],zombiePos[1]));
-	            }
-				sim.addZombies(zombies);
+		Scanner input;
+		try {
+			input = new Scanner(file);
 
-	            input.close();
-	            return sim;
-	            
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				return null;
-			}		
+			String line = "";
+
+			line = input.nextLine();
+			int[] size = SimulationState.stringToInt(line.split(" "));
+			Simulation sim = new Simulation(size[0], size[1]);
+
+			line = input.nextLine();
+			int[] playerPos = SimulationState.stringToInt(line.split(" "));
+			Point player = new Point(playerPos[0], playerPos[1]);
+			sim.addPlayer(player);
+
+			ArrayList<Point> zombies = new ArrayList<Point>();
+			while (input.hasNextLine()) {
+				line = input.nextLine();
+				int[] zombiePos = SimulationState.stringToInt(line.split(" "));
+				zombies.add(new Point(zombiePos[0], zombiePos[1]));
+			}
+			sim.addZombies(zombies);
+
+			input.close();
+			return sim;
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
 	}
 
-	
-static BotGen bot = new BotGen(2500, 2500, 42);
-	
+	static BotGen bot = new BotGen(2500, 2500, 42);
+
 	static void loop(Simulation sim, PrintWriter out) throws GameOver {
 		SimulationState simState = sim.getSimulationState();
 		bot.update(simState);
-		
-		if(bot.bomb) {
+
+		if (bot.bomb) {
 			System.out.println("true");
 			out.println("true");
 			sim.update(true);
-		}else {
+		} else {
 			int x = bot.x;
 			int y = bot.y;
-			
-			System.out.println(x + " " +y);
-			out.println(x + " " +y);
-			sim.update(new Point(x,y));
+
+			System.out.println(x + " " + y);
+			out.println(x + " " + y);
+			sim.update(new Point(x, y));
 		}
 
 	}
